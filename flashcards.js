94aacb02c67c700e7b475310eee01e0ad2a58d53
Ryan Cockram <ryan.cockram@ecutbildning.se>
2018-12-28 02:38:22 +0000

    updatedand documented some stuff

--- flashcards.js
+++ flashcards.js
@@ -1,3 +1,4 @@
+//When all DOM elements have finished loading, this function is called.
 $(document).on("ready", DOMLoadFunction());
 
 var CardList = [];
@@ -6,11 +7,15 @@ var currentCount = 0;
 function DOMLoadFunction() {
     $(".controllers").hide();
 
-    //assigns event handlers for DOM components
+    //assigns event handlers for currently existing DOM components
     $("#create").on("click", create_Click);
     $("#done").on("click", done_Click);
+    $("#save").on("click", save_Click);
+    $("#import").on("click", import_Click);
 }
 
+//Get the parent of the button (the card) 
+//to be able to delete the correct card.
 function delete_Click() {
     let child = $(this).parent().get(0).children[1].children[0].innerText;
     if (confirm("Are you sure you want to delete " +
@@ -19,6 +24,7 @@ function delete_Click() {
     }
 }
 
+//Hides the create button, and shows the create form.
 function create_Click() {
     $("#create").hide();
     $(".controllers").show();
@@ -26,6 +32,8 @@ function create_Click() {
     $("#reverse").val('');
 }
 
+//Adds the card, hiding the create form.
+//Checks to see if the fields are empty before proceeding
 function done_Click() {
     $("#create").show();
     $(".controllers").hide();
@@ -34,6 +42,14 @@ function done_Click() {
     
 }
 
+function save_Click() {
+    JSON.stringify(CardList);
+
+}
+
+//Operation for handling the showing of the two "sides" of the cards
+//Does this by getting the children of the currently clicked button's parent.
+//(this) being the button that was clicked.
 function flip_Click() {
     console.log(this);
     let title = $(this).parent().get(0).children[1].children[0];
@@ -50,6 +66,8 @@ function flip_Click() {
     }
 }
 
+//This just removes the card from the current array/list
+//To avoid future conflicting
 function removeCard(x) {
     CardList.forEach(Card => {
         if (x.id === Card.cardId) {
@@ -60,6 +78,9 @@ function removeCard(x) {
     $(x).remove();
 }
 
+//Adds the current card by appending a new set of html to the flashcard_container div
+//In this process, an object is created which is then added to the array/list "CardList"
+//This array/list is what is used when important and exporting data
 function addCard(title, reverse) {
     if (duplicationChecker())
         return;
@@ -82,12 +103,13 @@ function addCard(title, reverse) {
         "</div>"
     );
     $("#flip"+currentCount).on("click", flip_Click);
-    //$("#"+Card.cardId).data("index", {index: CardList.length})
     $("#delete"+currentCount).on("click", delete_Click);
     $("#back"+currentCount).hide();
     currentCount++;
 }
 
+//Performs an operation that checks if a card with the currently selected title exists
+//This needs to be avoided for reasons that would otherwise make the script unoperable
 function duplicationChecker() {
     let title = $("#title").val();
     let found = false;
@@ -101,6 +123,8 @@ function duplicationChecker() {
     return found;
 }
 
+//Checks if the fields of text are empty -- again, this is done to avoid things that would
+//make the script unoperable.
 function emptyChecker() {
     let title = $("#title").val();
     let reverse = $("#reverse").val();
@@ -115,3 +139,13 @@ function emptyChecker() {
     }
     return false;
 }
+
+function downloadObjectAsJson(exportObj, exportName) {
+    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
+    var downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute("href", dataStr);
+    downloadAnchorNode.setAttribute("download", exportName + ".json");
+    document.body.appendChild(downloadAnchorNode); // required for firefox
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+    }
